200cbd4d9ce7aa65e0c5d37f00c8758b4cc9563c
venkat.mareedu <venkat.mareedu@cgi.com>
2017-04-17 16:54:42 -0500

    Intial Commit

---
+++ JiraDemoTesting1/src/test/java/JiraTC02_Test.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JiraTC02_Test {
+	@Test
+	public void JiraTest1()
+	{
+		Assert.assertEquals("expeted", "expeted");
+		System.out.println("JIRA Integration Testing Demo");
+	}
+}
