d81ea22b5a95a0bb6e1e688f4e0ab57f6c517a1f
venkat.mareedu <venkat.mareedu@cgi.com>
2017-04-18 14:20:13 -0500

    intial commit

--- JiraDemoTesting1/src/test/java/JiraTC001_Test.java
+++ JiraDemoTesting1/src/test/java/JiraTC001_Test.java
@@ -1,11 +1,11 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class JiraTC01_Test {
+public class JiraTC001_Test {
 	@Test
 	public void JiraTest1()
 	{
-		Assert.assertEquals("expeted", "expeted");
+		Assert.assertEquals("expeted", "123");
 		System.out.println("JIRA Integration Testing Demo");
 	}
 }
--- JiraDemoTesting1/src/test/java/JiraTC003_Test.java
+++ JiraDemoTesting1/src/test/java/JiraTC003_Test.java
@@ -1,11 +1,11 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class JiraTC03_Test {
+public class JiraTC003_Test {
 	@Test
 	public void JiraTest1()
 	{
-		Assert.assertEquals("expeted", "expeted");
+		Assert.assertEquals("expeted", "Actual");
 		System.out.println("JIRA Integration Testing Demo");
 	}
 }
